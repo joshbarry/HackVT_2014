1c6ca5e0f59501915a5ba77ce3600a73c1195e05
joshbarry92 <joshbarry92@gmail.com>
2014-10-10 19:43:58 -0400

    honestly dont know

--- Web/index.php
+++ Web/index.php
@@ -10,18 +10,14 @@
 	<link rel="shortcut icon" href="img/icon.ico">
 	<link href="css/mainStyle.css" type="text/css" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.4.4/jquery.mobile-1.4.4.min.js"></script>
-	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.4/jquery.mobile-1.4.4.min.css">
-	<script src="./js/jquery-min.js"></script>
 </head>
 
 <body class="sf-home">
 	
 	<form>
 	First name: <input type="text" name="firstname"><br>
-	City, State (Home):<input type="text" name="home-address"><br>
-	City, State (Work):<input type="text" name="work-address"><br>
+	City (Home):<input type="text" name="home-address"><br>
+	City (Work):<input type="text" name="work-address"><br>
 	Rent or Buy: <div class="checkRent">
   		<input type="checkbox" value="1" id="checkRent" name="checkRent"  style="display:none"/>
 	  	<label for="checkRent"></label></div><br><br>
@@ -48,6 +44,7 @@
 	<?php //insert SQL code to update anchor text ?>
 	Power Usage: <?php echo "Anchor 1" ?><input type="range" name="powerusage" id="powerusage" value="50" min="0" max="100"><?php echo "Anchor 2" ?><br>
 	</form>
+
 </body>
 
 </html>
