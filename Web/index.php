e51c05d4d1a78ebe10992900ccaa6d5827ceb0e8
joshbarry92 <joshbarry92@gmail.com>
2014-10-10 18:13:15 -0400

    css

--- Web/index.php
+++ Web/index.php
@@ -8,7 +8,7 @@
 <head>
 	<title>HackVT 2014</title>
 	<link rel="shortcut icon" href="img/icon.ico">
-	<link href="styleMain.css" type="text/css" rel="stylesheet">
+	<link href="css/styleMain.css" type="text/css" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="./js/jquery-min.js"></script>
 </head>
@@ -17,9 +17,10 @@
 
 	<form>
 	First name: <input type="text" name="firstname"><br>
-	City, State (Home):<input type="text" name="home-address">
-	City, State (Work):<input type="text" name="work-address">
-	Rent or Buy?: <input type="checkbox" data-role="flipswitch" name="ownership" id="switch">
+	City, State (Home):<input type="text" name="home-address"><br>
+	City, State (Work):<input type="text" name="work-address"><br>
+	Rent or Buy: <input type="checkbox" data-role="flipswitch" class="checkRent" name="rentbuy" id="switch"><br>
+	How Long Will You Stay:
 	Current Heat Source:<select name="heat">
 		<option value="wood">Wood</option>
 		<option value="pellet">Pellet Stove</option>
@@ -28,15 +29,15 @@
 		<option value="steam">Steam</option>
 		<option value="geothermal">Geothermal</option>
 		<option value="baseboard">Baseboard</option>
-		</select>
-	Heat Usage: <input type="range" name="heatusage" id="heatusage" value="50" min="0" max="100">
+		</select><br>
+	Heat Usage: <input type="range" name="heatusage" id="heatusage" value="50" min="0" max="100"><br>
 	Type of Power<select name="power">
 		<option value="electric">Electric (Grid)</option>
 		<option value="solar">Solar</option>
 		<option value="wind">Wind</option>
 		<option value="hydro">Hydro</option>
-		</select>
-	Power Usage: <input type="range" name="powerusage" id="powerusage" value="50" min="0" max="100">
+		</select><br>
+	Power Usage: <input type="range" name="powerusage" id="powerusage" value="50" min="0" max="100"><br>
 	</form>
 </body>
 
