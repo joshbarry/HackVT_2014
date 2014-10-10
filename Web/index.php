08f2421c40fc765607611f7f76c63534a13eb44e
joshbarry92 <joshbarry92@gmail.com>
2014-10-10 17:50:40 -0400

    update slider

--- Web/index.php
+++ Web/index.php
@@ -19,16 +19,24 @@
 	First name: <input type="text" name="firstname"><br>
 	City, State (Home):<input type="text" name="home-address">
 	City, State (Work):<input type="text" name="work-address">
-	Current Heat Source:<select name="cars">
+	Rent or Buy?: <input type="checkbox" data-role="flipswitch" name="ownership" id="switch">
+	Current Heat Source:<select name="heat">
 		<option value="wood">Wood</option>
 		<option value="pellet">Pellet Stove</option>
 		<option value="natural gas">Natural Gas</option>
 		<option value="oil">Oil</option>
+		<option value="steam">Steam</option>
 		<option value="geothermal">Geothermal</option>
 		<option value="baseboard">Baseboard</option>
 		</select>
-	Type of Power<input ...>
-	
+	Heat Usage: <input type="range" name="heatusage" id="heatusage" value="50" min="0" max="100">
+	Type of Power<select name="power">
+		<option value="electric">Electric (Grid)</option>
+		<option value="solar">Solar</option>
+		<option value="wind">Wind</option>
+		<option value="hydro">Hydro</option>
+		</select>
+	Power Usage: <input type="range" name="powerusage" id="powerusage" value="50" min="0" max="100">
 	</form>
 </body>
 
