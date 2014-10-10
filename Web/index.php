f72890f377d4cc5cd8c559f66a8b2edfa910c9ab
joshbarry92 <joshbarry92@gmail.com>
2014-10-10 18:58:35 -0400

    indez

--- Web/index.php
+++ Web/index.php
@@ -8,7 +8,7 @@
 <head>
 	<title>HackVT 2014</title>
 	<link rel="shortcut icon" href="img/icon.ico">
-	<link href="css/styleMain.css" type="text/css" rel="stylesheet">
+	<link href="css/mainStyle.css" type="text/css" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="./js/jquery-min.js"></script>
 </head>
@@ -19,8 +19,10 @@
 	First name: <input type="text" name="firstname"><br>
 	City, State (Home):<input type="text" name="home-address"><br>
 	City, State (Work):<input type="text" name="work-address"><br>
-	Rent or Buy: <input type="checkbox" data-role="flipswitch" class="checkRent" name="rentbuy" id="switch"><br>
-	How Long Will You Stay:
+	Rent or Buy: <div class="checkRent">
+  		<input type="checkbox" value="1" id="checkRent" name="checkRent"  style="display:none"/>
+	  	<label for="checkRent"></label></div><br><br>
+	How Long Will You Stay:<input type="range" name="years" id="years" value="5" min="0" max="80"><br>
 	Current Heat Source:<select name="heat">
 		<option value="wood">Wood</option>
 		<option value="pellet">Pellet Stove</option>
@@ -30,14 +32,15 @@
 		<option value="geothermal">Geothermal</option>
 		<option value="baseboard">Baseboard</option>
 		</select><br>
-	Heat Usage: <input type="range" name="heatusage" id="heatusage" value="50" min="0" max="100"><br>
+	<?php //insert SQL code to update anchor text ?>
+	Heat Usage: <?php echo "Anchor 1" ?><input type="range" name="heatusage" id="heatusage" value="50" min="0" max="100"><?php echo "Anchor 2" ?><br>
 	Type of Power<select name="power">
 		<option value="electric">Electric (Grid)</option>
 		<option value="solar">Solar</option>
 		<option value="wind">Wind</option>
 		<option value="hydro">Hydro</option>
 		</select><br>
-	Power Usage: <input type="range" name="powerusage" id="powerusage" value="50" min="0" max="100"><br>
+	Power Usage: <?php echo "Anchor 1" ?><input type="range" name="powerusage" id="powerusage" value="50" min="0" max="100"><?php echo "Anchor 2" ?><br>
 	</form>
 </body>
 
