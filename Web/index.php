9bfea21c4ada9bbc2b94c24855c0ec5b5f2e3d51
Tyler J. Sawyer <aayrlgrump@gmail.com>
2014-10-11 01:12:31 -0400

    Styles are ready for work later on.
    Make it pretty as fuck.

--- Web/index.php
+++ Web/index.php
@@ -21,88 +21,90 @@ while($row = mysqli_fetch_array($resultAnchor)) {
 </head>
 
 <body class="sf-home">
-	<div id="content-left">
-		<form>
-		<div class="viewItem">
-			<div class="viewTitle">First name:</div>
-			<div class="viewField"><input type="text" name="firstname"></div>
-		</div>
-		<div class="viewItem">
-			<div class="viewTitle">City (Home):</div>
-			<div class="viewField"><input type="text" name="home-address"></div>
-		</div>
-		<div class="viewItem">
-			<div class="viewTitle">City (Work):</div>
-			<div class="viewField"><input type="text" name="work-address"></div>
-		</div>
-		
-		<div class="viewItem">
-			<div class="viewTitle">Mode of Transport:</div>
-			<div class="viewField">
-				<select id="transport" name="transport">
-					<option value="-1" disabled="disabled" selected="selected">Select a Method of Transportation</option>
-					<option value="electric">Walk</option>
-					<option value="solar">Bike</option>
-					<option value="wind">Drive</option>
-				</select>
+	<div id="content">
+		<div id="content-left">
+			<form>
+			<div class="viewItem">
+				<div class="viewTitle">First name:</div>
+				<div class="viewField"><input type="text" name="firstname"></div>
 			</div>
-		</div>
-		<div id="transportDiv"></div>
-		
-		<div class="viewItem">
-			<div class="viewTitle">Rent or Buy:</div>
-			<div class="viewField">
-				<div class="checkRent">
-					<input type="checkbox" value="1" id="checkRent" name="checkRent"  style="display:none"/>
-					<label for="checkRent"></label>
+			<div class="viewItem">
+				<div class="viewTitle">City (Home):</div>
+				<div class="viewField"><input type="text" name="home-address"></div>
+			</div>
+			<div class="viewItem">
+				<div class="viewTitle">City (Work):</div>
+				<div class="viewField"><input type="text" name="work-address"></div>
+			</div>
+			
+			<div class="viewItem">
+				<div class="viewTitle">Mode of Transport:</div>
+				<div class="viewField">
+					<select id="transport" name="transport">
+						<option value="-1" disabled="disabled" selected="selected">Select a Method of Transportation</option>
+						<option value="electric">Walk</option>
+						<option value="solar">Bike</option>
+						<option value="wind">Drive</option>
+					</select>
 				</div>
 			</div>
-		</div>
+			<div id="transportDiv"></div>
 			
-		<div class="viewItem">
-			<div class="viewTitle">How Long Will You Stay:</div>
-			<div class="viewField">
-				<input type="range" name="years" id="years" value="5" data-popup-enabled="true" data-hightlight="true" min="0" max="80">
+			<div class="viewItem">
+				<div class="viewTitle">Rent or Buy:</div>
+				<div class="viewField">
+					<div class="checkRent">
+						<input type="checkbox" value="1" id="checkRent" name="checkRent"  style="display:none"/>
+						<label for="checkRent"></label>
+					</div>
+				</div>
 			</div>
-		</div>
-		
-		<div class="viewItem">
-			<div class="viewTitle">Current Heat Source:</div>
-			<div class="viewField">
-				<select name="heat" id="heat">
-					<option value="-1" disabled="disabled" selected="selected">Select a Heat Source</option>
-					<option value="wood">Wood</option>
-					<option value="pellet">Pellet Stove</option>
-					<option value="natural gas">Natural Gas</option>
-					<option value="oil">Oil</option>
-					<option value="propane">Propane</option>
-					<option value="kerosene">Kerosene</option>
-					<option value="geothermal">Geothermal</option>
-					<option value="heatPump">Heat Pump</option>
-					<option value="baseboard">Baseboard</option>
-				</select>
+				
+			<div class="viewItem">
+				<div class="viewTitle">How Long Will You Stay:</div>
+				<div class="viewField">
+					<input type="range" name="years" id="years" value="5" data-popup-enabled="true" data-hightlight="true" min="0" max="80">
+				</div>
 			</div>
-		</div>
-		<div id="heatUse"></div>
-		
-		<div class="viewItem">
-			<div class="viewTitle">Type of Power:</div>
-			<div class="viewField">
-				<select id="power" name="power">
-					<option value="-1" disabled="disabled" selected="selected">Select a Power Source</option>
-					<option value="electric">Electric (Grid)</option>
-					<option value="solar">Solar</option>
-					<option value="wind">Wind</option>
-					<option value="hydro">Hydro</option>
-				</select><br>
+			
+			<div class="viewItem">
+				<div class="viewTitle">Current Heat Source:</div>
+				<div class="viewField">
+					<select name="heat" id="heat">
+						<option value="-1" disabled="disabled" selected="selected">Select a Heat Source</option>
+						<option value="wood">Wood</option>
+						<option value="pellet">Pellet Stove</option>
+						<option value="natural gas">Natural Gas</option>
+						<option value="oil">Oil</option>
+						<option value="propane">Propane</option>
+						<option value="kerosene">Kerosene</option>
+						<option value="geothermal">Geothermal</option>
+						<option value="heatPump">Heat Pump</option>
+						<option value="baseboard">Baseboard</option>
+					</select>
+				</div>
 			</div>
+			<div id="heatUse"></div>
+			
+			<div class="viewItem">
+				<div class="viewTitle">Type of Power:</div>
+				<div class="viewField">
+					<select id="power" name="power">
+						<option value="-1" disabled="disabled" selected="selected">Select a Power Source</option>
+						<option value="electric">Electric (Grid)</option>
+						<option value="solar">Solar</option>
+						<option value="wind">Wind</option>
+						<option value="hydro">Hydro</option>
+					</select><br>
+				</div>
+			</div>
+			<div id="powerUse"></div>
+			
+			</form>
 		</div>
-		<div id="powerUse"></div>
-		
-		</form>
-	</div>
-	<div id="content-right">
-		A BIG HONKING CHART GOES HERE
+		<div id="content-right">
+			A BIG HONKING CHART GOES HERE
+		</div>
 	</div>
 </body>
 
