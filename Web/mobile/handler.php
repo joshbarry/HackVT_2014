d05625cd6e70c85a7eba9747f444add0db026753
Tyler J. Sawyer <aayrlgrump@gmail.com>
2014-10-10 21:34:22 -0400

    Handler for Mobile Devices
    Handler...for my knuts.

---
+++ Web/mobile/handler.php
@@ -0,0 +1,49 @@
+<?php 
+/**
+ * @package		SFHackVT
+ * @subpackage	MOBILE_HANDLER
+ * 
+ * @author		Tyler J. Sawyer
+ * @date		2014-10-10 21:01:36
+ * @version		1.0.0
+ * @copyright	Copyright (C) Tyler J. Sawyer, All Rights Reserved.
+ */
+
+require_once( '../support.php' );
+ 
+// Request some data from our database.
+if ($_GET['bacon']=="yes") {
+	
+	// Retrieve details from DB.
+	$connector = dbconnect();
+	$grabtypes_stmt = $connector->prepare('SELECT * FROM anchor');
+	$grabtypes_stmt->execute();
+	$results = dbGetResultRows($grabtypes_stmt);
+	if(is_array($results)) {
+		foreach($results as $row) {
+			$ttype = $row['type'];
+			$ta1 = $row['a1'];
+			$ta2 = $row['a2'];
+			$tmin = $row['min'];
+			$tmax = $row['max'];
+		}
+	}
+	dbdisconnect($connector);
+
+	$finalResponse = array(
+		"Type" => $ttype,
+		"Anchor1" => $ta1,
+		"Anchor2" => $ta2,
+		"MinValue" => $tmin,
+		"MaxValue" => $tmax
+	);
+	
+	// Send Output
+	echo json_encode($finalResponse);
+	
+} else {
+	$data['error'] = 'ERROR_999: SERVER UNAVAILABLE.';
+	echo json_encode($data);
+}
+
+?>
