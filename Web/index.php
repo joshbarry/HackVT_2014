08ace4e438f5cea2fb1cd618840adae9fc290aa7
Tyler J. Sawyer <aayrlgrump@gmail.com>
2014-10-11 00:56:52 -0400

    Added Index.php Stylings
    Making it purty.

--- Web/index.php
+++ Web/index.php
@@ -21,46 +21,86 @@ while($row = mysqli_fetch_array($resultAnchor)) {
 </head>
 
 <body class="sf-home">
-	
-	<form>
-	First name: <input type="text" name="firstname"><br>
-	City (Home):<input type="text" name="home-address"><br>
-	City (Work):<input type="text" name="work-address"><br>
-	Mode of Transport: <select id="transport" name="transport">
-		<option value="-1" disabled="disabled" selected="selected">Select a Method of Transportation</option>
-		<option value="electric">Walk</option>
-		<option value="solar">Bike</option>
-		<option value="wind">Drive</option>
-		</select><br>
-	<div id="transportDiv"> </div>
-	Rent or Buy: <div class="checkRent">
-  		<input type="checkbox" value="1" id="checkRent" name="checkRent"  style="display:none"/>
-	  	<label for="checkRent"></label></div><br><br>
-	How Long Will You Stay:<input type="range" name="years" id="years" value="5" data-popup-enabled="true" data-hightlight="true" min="0" max="80"><br>
-	Current Heat Source:<select name="heat" id="heat">
-		<option value="-1" disabled="disabled" selected="selected">Select a Heat Source</option>
-		<option value="wood">Wood</option>
-		<option value="pellet">Pellet Stove</option>
-		<option value="natural gas">Natural Gas</option>
-		<option value="oil">Oil</option>
-		<option value="propane">Propane</option>
-		<option value="kerosene">Kerosene</option>
-		<option value="geothermal">Geothermal</option>
-		<option value="heatPump">Heat Pump</option>
-		<option value="baseboard">Baseboard</option>
-		</select><br>
-	<div id="heatUse"> </div>
-	Type of Power<select id="power" name="power">
-		<option value="-1" disabled="disabled" selected="selected">Select a Power Source</option>
-		<option value="electric">Electric (Grid)</option>
-		<option value="solar">Solar</option>
-		<option value="wind">Wind</option>
-		<option value="hydro">Hydro</option>
-		</select><br>
-	<div id="powerUse"> </div>
-	
-	</form>
-
+	<div id="content">
+		<form>
+		<div class="viewItem">
+			<div class="viewTitle">First name:</div>
+			<div class="viewField"><input type="text" name="firstname"></div>
+		</div>
+		<div class="viewItem">
+			<div class="viewTitle">City (Home):</div>
+			<div class="viewField"><input type="text" name="home-address"></div>
+		</div>
+		<div class="viewItem">
+			<div class="viewTitle">City (Work):</div>
+			<div class="viewField"><input type="text" name="work-address"></div>
+		</div>
+		
+		<div class="viewItem">
+			<div class="viewTitle">Mode of Transport:</div>
+			<div class="viewField">
+				<select id="transport" name="transport">
+					<option value="-1" disabled="disabled" selected="selected">Select a Method of Transportation</option>
+					<option value="electric">Walk</option>
+					<option value="solar">Bike</option>
+					<option value="wind">Drive</option>
+				</select>
+			</div>
+		</div>
+		<div id="transportDiv"></div>
+		
+		<div class="viewItem">
+			<div class="viewTitle">Rent or Buy:</div>
+			<div class="viewField">
+				<div class="checkRent">
+					<input type="checkbox" value="1" id="checkRent" name="checkRent"  style="display:none"/>
+					<label for="checkRent"></label>
+				</div>
+			</div>
+		</div>
+			
+		<div class="viewItem">
+			<div class="viewTitle">How Long Will You Stay:</div>
+			<div class="viewField">
+				<input type="range" name="years" id="years" value="5" data-popup-enabled="true" data-hightlight="true" min="0" max="80">
+			</div>
+		</div>
+		
+		<div class="viewItem">
+			<div class="viewTitle">Current Heat Source:</div>
+			<div class="viewField">
+				<select name="heat" id="heat">
+					<option value="-1" disabled="disabled" selected="selected">Select a Heat Source</option>
+					<option value="wood">Wood</option>
+					<option value="pellet">Pellet Stove</option>
+					<option value="natural gas">Natural Gas</option>
+					<option value="oil">Oil</option>
+					<option value="propane">Propane</option>
+					<option value="kerosene">Kerosene</option>
+					<option value="geothermal">Geothermal</option>
+					<option value="heatPump">Heat Pump</option>
+					<option value="baseboard">Baseboard</option>
+				</select>
+			</div>
+		</div>
+		<div id="heatUse"></div>
+		
+		<div class="viewItem">
+			<div class="viewTitle">Type of Power:</div>
+			<div class="viewField">
+				<select id="power" name="power">
+					<option value="-1" disabled="disabled" selected="selected">Select a Power Source</option>
+					<option value="electric">Electric (Grid)</option>
+					<option value="solar">Solar</option>
+					<option value="wind">Wind</option>
+					<option value="hydro">Hydro</option>
+				</select><br>
+			</div>
+		</div>
+		<div id="powerUse"></div>
+		
+		</form>
+	</div>
 </body>
 
 </html>
