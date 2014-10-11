631ca31ccdec1521aef21947f3a7b148930a6889
Tyler J. Sawyer <aayrlgrump@gmail.com>
2014-10-11 07:40:03 -0400

    THE GRAPH IS DONE
    PRAISE RAPTOR JESUS

--- Web/jquery-graphy.php
+++ Web/jquery-graphy.php
@@ -38,22 +38,25 @@ function makifyGraph( month = 1, cost = 0 ){
 	var months = [ "January", "February", "March", "April", "May", "June", 
                "July", "August", "September", "October", "November", "December" ];
 	var selectedMonthName = months[month-1];
+	var lineWind = [];
+	var lineHydro = [];
+	var lineSolar = [];
 	<?php
 	for ($i=0; $i<12; $i++) {
 		$monthData = $arrayMonthDatas[$i];
-		$bacon .= 'var lineWind'.$i.' = [';
+		$bacon .= 'lineWind['.$i.'] = [';
 		foreach ($monthData[0] as $windy) {
 			$bacon .= '"'.$windy.'", ';
 		}
 		$bacon = substr($bacon, 0, -2);
 		$bacon .= '];';
-		$bacon .= 'var lineHydro'.$i.' = [';
+		$bacon .= 'lineHydro['.$i.'] = [';
 		foreach ($monthData[1] as $hydro) {
 			$bacon .= '"'.$hydro.'", ';
 		}
 		$bacon = substr($bacon, 0, -2);
 		$bacon .= '];';
-		$bacon .= 'var lineSolar'.$i.' = [';
+		$bacon .= 'lineSolar['.$i.'] = [';
 		foreach ($monthData[2] as $solar) {
 			$bacon .= '"'.(int)$solar.'", ';
 		}
@@ -66,7 +69,7 @@ function makifyGraph( month = 1, cost = 0 ){
 	for (var i=0; i<31; i+=1){
 		lineAvgCost.push([i, cost]);
 	}
-	var plot2 = $.jqplot ('chartdiv', [lineSolar0, lineHydro0, lineWind0, lineAvgCost], {
+	var plot2 = $.jqplot ('chartdiv', [lineSolar[month-1], lineHydro[month-1], lineWind[month-1], lineAvgCost], {
       // Give the plot a title.
       title: 'Averages for ' + selectedMonthName,
       // You can specify options for all axes on the plot at once with
@@ -104,7 +107,7 @@ function makifyGraph( month = 1, cost = 0 ){
         yaxis: {
           label: "Daily KW/H Usage (AVG)",
 		  min: 0,
-		  max: 150
+		  max: 50
         }
       }
     });
